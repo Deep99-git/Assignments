f9f0fbd2566646889afbf38632912f8f61804adc
Deep99-git <67640845+Deep99-git@users.noreply.github.com>
2021-07-22 18:23:24 +0530

    first commit

---
+++ toady_assign/assignment.js
@@ -0,0 +1,13 @@
+function big(img){
+    img.style.width = "400px"
+    img.style.hight = "500px"
+}
+function normal(img){
+    img.style.width = "300px"
+    img.style.hight = "300px"
+}
+
+function show(){
+    document.getElementById("image").src = "img2.jpg"
+}
+
