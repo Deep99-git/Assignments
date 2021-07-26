0172ba47b8efa4ec9956a1a148f1d2ad2298c353
Deep99-git <67640845+Deep99-git@users.noreply.github.com>
2021-07-26 14:08:07 +0530

    updated

--- toady_assign/assignment.js
+++ toady_assign/assignment.js
@@ -11,3 +11,14 @@ function show(){
     document.getElementById("image").src = "img2.jpg"
 }
 
+function show_four(){
+    document.getElementById("four").style.display = "block"
+}
+
+function hide_four(){
+    document.getElementById("four").style.display = "none"
+}
+function change(){
+    document.getElementById("second").style.background = 'linear-gradient(to right,cadetblue,aquamarine)'
+    document.getElementById("para").style.color = 'yellow'
+}
